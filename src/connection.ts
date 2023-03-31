f2153d139195f5ef1e1cb64fb50811feeb3e5073
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-03-31 16:59:34 +0200

    fix: remove server.listen

--- src/connection.ts
+++ src/connection.ts
@@ -10,29 +10,26 @@ import type { CoapRequestParams} from 'coap'
 
 const server = coap.createServer()
 
-server.listen(() => {
-   
-    const creationRequest: CoapRequestParams = {
-        host: config.host,
-        port: config.port,
-        method: "POST",
-        pathname: '/rd',
-        query: 'ep=' + config.deviceName + '&lt=' + config.lifetime + '&lwm2m=' + config.lwm2mV + '&b=' + config.biding 
-    }
+const creationRequest: CoapRequestParams = {
+    host: config.host,
+    port: config.port,
+    method: "POST",
+    pathname: '/rd',
+    query: 'ep=' + config.deviceName + '&lt=' + config.lifetime + '&lwm2m=' + config.lwm2mV + '&b=' + config.biding 
+}
 
-    const req = coap.request(creationRequest)
+const req = coap.request(creationRequest)
 
-    const assetTrackerObjects = "</3/0>, <50009/0>, <3303/0>, <3304/0>, <3323/0>, </6/0>, </4/0>"
+const assetTrackerObjects = "</3/0>, <50009/0>, <3303/0>, <3304/0>, <3323/0>, </6/0>, </4/0>"
 
-    req.write(assetTrackerObjects, () =>{
-        console.log("Payload sent: ", assetTrackerObjects)
-    })
-
-    req.on('response', (res: { code: string; payload: object }) => {
-        console.log('Server response with: ' + res.code);
-        console.log(res)
-        //res.pipe(process.stdout)
-    })
-
-   req.end()
+req.write(assetTrackerObjects, () =>{
+    console.log("Payload sent: ", assetTrackerObjects)
 })
+
+req.on('response', (res: { code: string; payload: object }) => {
+    console.log('Server response with: ' + res.code);
+    console.log(res)
+    //res.pipe(process.stdout)
+})
+
+req.end()
