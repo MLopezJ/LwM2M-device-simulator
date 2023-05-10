1bbb3ea8c6abe504f8732c82477d026d5c410a8f
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-05-10 17:32:32 +0200

    fix: remove bootstrap file
    Not needed anymore

--- src/cmds/bootstrap.ts
+++
@@ -1,8 +0,0 @@
-import { assetTrackerFirmwareV2 } from "../assetTrackerV2.js"
-import type { assetTracker } from "../assetTrackerV2.js";
-import type { Agent } from 'coap'
-import coap from 'coap'
-
-const defaultType = "udp4"
-const createAgent = () => new coap.Agent({type:defaultType})
-export const bootstrap = (): [assetTracker, Agent] => [structuredClone(assetTrackerFirmwareV2),createAgent()]
