f37f695abd44585e9e3fce40e21fb08a4aa906eb
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-05-12 13:18:23 +0200

    feat: check if resource exist in object

---
+++ src/utils/checkResource.test.ts
@@ -0,0 +1,23 @@
+import { Device_3_urn, Temperature_3303_urn } from "@nordicsemiconductor/lwm2m-types"
+import { assetTrackerFirmwareV2, type assetTracker } from "../assetTrackerV2"
+import { checkResource } from "./checkResource"
+
+describe("Check Resource", () => {
+    it("Should return resource value if resource exist", () => {
+        expect(checkResource(assetTrackerFirmwareV2[Device_3_urn]!, 0)).toBe("Nordic")
+    })
+    
+    it.each([
+        [ 1, "Device", assetTrackerFirmwareV2[Device_3_urn]],
+        [ 5700, "Temperature", assetTrackerFirmwareV2[Temperature_3303_urn]![0]]
+    ])("Should check that resource %p exist in object: %p", (resourceId: number, objectName: string, object: unknown, ) => {
+        expect(checkResource(object as assetTracker, resourceId)).not.toBe(undefined)
+    })
+
+    it.each([
+        [ 404, "Device", assetTrackerFirmwareV2[Device_3_urn]],
+        [ 10101010, "Temperature", assetTrackerFirmwareV2[Temperature_3303_urn]![0]]
+    ])("Should check that resource %p do not exist in object: %p", (resourceId: number, objectName: string, object: unknown, ) => {
+        expect(checkResource(object as Partial<assetTracker>, resourceId)).toBe(undefined)
+    })
+})
---
+++ src/utils/checkResource.ts
@@ -0,0 +1,4 @@
+/**
+ * Check if resource exist in object
+ */
+export const checkResource = (object: Record<string, unknown>, resourceId: number): unknown =>  object[`${resourceId}`]
