b4f0192d12bd2b45da887ff9c28a22d0c96bf233
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-05-12 13:17:31 +0200

    feat: Check if instance exist in object

---
+++ src/utils/checkInstance.ts
@@ -0,0 +1,21 @@
+
+export type instance = Record<string, unknown> | Record<string, unknown>[]
+
+/**
+ * Check if instance exist in object
+ */
+export const checkInstance = (object: instance, instanceId: number): undefined | Record<string, unknown> => {
+    // single instance
+    if (Array.isArray(object) === false){
+      if(instanceId > 0) return undefined
+  
+      // @ts-ignore
+      return object
+    }
+  
+    // repeat list validation to satisfy typescript concern about type 'number' can't be used to index type 'Partial<assetTracker>'.
+    return Array.isArray(object) ? object[instanceId] : undefined
+
+
+  }
+  
