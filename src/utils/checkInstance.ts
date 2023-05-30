7c7cc4a6d9772311ce324766d250a59612cb229f
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-05-30 17:43:59 +0200

    fix: type specification

--- src/utils/checkInstance.ts
+++ src/utils/checkInstance.ts
@@ -9,8 +9,7 @@ export const checkInstance = (object: instance, instanceId: number): undefined |
     if (Array.isArray(object) === false){
       if(instanceId > 0) return undefined
   
-      // @ts-ignore
-      return object
+      return object as Record<string, unknown>
     }
   
     // repeat list validation to satisfy typescript concern about type 'number' can't be used to index type 'Partial<assetTracker>'.
