f39d246c9162ecc1fcde4ddcafa8c2cb867976cc
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-05-31 10:12:13 +0200

    fix: import type

--- src/utils/checkInstance.test.ts
+++ src/utils/checkInstance.test.ts
@@ -1,6 +1,6 @@
 import { Device_3_urn, Temperature_3303_urn } from "@nordicsemiconductor/lwm2m-types"
 import { assetTrackerFirmwareV2 } from "../assetTrackerV2.js"
-import { checkInstance, instance } from "./checkInstance.js"
+import { checkInstance, type instance } from "./checkInstance.js"
 
 describe("Check Instance", () => {
     it("Should check that instance exist in object (single instance object)", () => {
