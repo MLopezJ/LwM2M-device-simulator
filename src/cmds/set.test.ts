f50c750598cf7f99d45725dad6625592ae4327d9
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-06-07 13:40:40 +0200

    test: update set command test

--- src/cmds/set.test.ts
+++ src/cmds/set.test.ts
@@ -2,9 +2,10 @@ import jest from 'jest-mock'
 import { assetTrackerFirmwareV2, type assetTracker } from '../assetTrackerV2'
 import { type element } from '../utils/getElementPath'
 import { set } from './set'
+import { Device_3_urn } from '@nordicsemiconductor/lwm2m-types'
 
 describe('Set command', () => {
-	it('should update resource with value and register it in server', () => {
+	it('should update resource with value, register it in server and return list with new value as part of the state of Device Objects', () => {
 		const userInput = ['/3/0/0', 'Mauro']
 		const url = userInput[0]
 		const newValue = userInput[1]
@@ -13,32 +14,49 @@ describe('Set command', () => {
 			instanceId: 0,
 			resourceId: 0,
 		}
+		const deviceObjects = assetTrackerFirmwareV2
 
 		const getPath = jest.fn().mockReturnValue(path) as () => element
+		
 		const changeResourceValue = jest
 			.fn()
-			.mockReturnValue(assetTrackerFirmwareV2) as () => assetTracker
+			.mockImplementationOnce(() => {
+				const newDeviceObjects = structuredClone(deviceObjects)
+				// @ts-ignore: TODO: solve this. Remove readonly
+				newDeviceObjects[Device_3_urn]?.['0'] = newValue 
+				return newDeviceObjects
+			}) as () => assetTracker
+
 		const registerNewValue = jest.fn()
 
-		set(
+		const result = set(
 			userInput,
-			assetTrackerFirmwareV2,
+			deviceObjects,
 			getPath,
 			changeResourceValue,
 			registerNewValue,
 		)
 
+		// identify object id, resource id and resource id from url
 		expect(getPath).toHaveBeenCalledWith(url)
+
+
+		// update the value in Device Objects
 		expect(changeResourceValue).toHaveBeenCalledWith(
-			assetTrackerFirmwareV2,
+			deviceObjects,
 			path,
 			newValue,
 		)
 
 		const bracket = `<${url}>`
+		// register new resource in LwM2M Server
 		expect(registerNewValue).toHaveBeenCalledWith(
 			bracket,
-			assetTrackerFirmwareV2,
+			result,
 		)
+
+		// expect to return a new state of Device Objects
+		expect(result?.[Device_3_urn]?.['0']).toBe(newValue)
+		expect(deviceObjects?.[Device_3_urn]?.['0']).not.toBe(newValue)
 	})
 })
