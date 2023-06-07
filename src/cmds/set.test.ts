571ea993a53ae53fbef51a4e5491db8e75361825
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-06-07 14:00:52 +0200

    test: use updateResource method

--- src/cmds/set.test.ts
+++ src/cmds/set.test.ts
@@ -1,8 +1,9 @@
+import { Device_3_urn } from '@nordicsemiconductor/lwm2m-types'
 import jest from 'jest-mock'
 import { assetTrackerFirmwareV2, type assetTracker } from '../assetTrackerV2'
 import { type element } from '../utils/getElementPath'
+import { updateResource } from '../utils/updateResource.js'
 import { set } from './set'
-import { Device_3_urn } from '@nordicsemiconductor/lwm2m-types'
 
 describe('Set command', () => {
 	it('should update resource with value, register it in server and return list with new value as part of the state of Device Objects', () => {
@@ -17,15 +18,17 @@ describe('Set command', () => {
 		const deviceObjects = assetTrackerFirmwareV2
 
 		const getPath = jest.fn().mockReturnValue(path) as () => element
-		
-		const changeResourceValue = jest
-			.fn()
-			.mockImplementationOnce(() => {
+
+		const changeResourceValue = jest.fn().mockImplementationOnce(() => {
+			const newDeviceObjects = structuredClone(deviceObjects)
+			return updateResource(newDeviceObjects, path, newValue ?? '')
+			/*
 				const newDeviceObjects = structuredClone(deviceObjects)
 				// @ts-ignore: TODO: solve this. Remove readonly
 				newDeviceObjects[Device_3_urn]?.['0'] = newValue 
 				return newDeviceObjects
-			}) as () => assetTracker
+				*/
+		}) as () => assetTracker
 
 		const registerNewValue = jest.fn()
 
@@ -40,7 +43,6 @@ describe('Set command', () => {
 		// identify object id, resource id and resource id from url
 		expect(getPath).toHaveBeenCalledWith(url)
 
-
 		// update the value in Device Objects
 		expect(changeResourceValue).toHaveBeenCalledWith(
 			deviceObjects,
@@ -50,10 +52,7 @@ describe('Set command', () => {
 
 		const bracket = `<${url}>`
 		// register new resource in LwM2M Server
-		expect(registerNewValue).toHaveBeenCalledWith(
-			bracket,
-			result,
-		)
+		expect(registerNewValue).toHaveBeenCalledWith(bracket, result)
 
 		// expect to return a new state of Device Objects
 		expect(result?.[Device_3_urn]?.['0']).toBe(newValue)
