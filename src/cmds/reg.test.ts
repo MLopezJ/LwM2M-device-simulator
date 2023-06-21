9f5d5d6fb3e521e79b470ba152a3ec4f5f150f11
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-06-21 14:32:05 +0200

    test(register): set globals

--- src/cmds/reg.test.ts
+++ src/cmds/reg.test.ts
@@ -4,12 +4,19 @@ import { type CoapMethod } from 'coap-packet'
 
 describe("handshake", () => {
     let server: Server
-    let port: number
+
+    const agent = new coap.Agent({ type: 'udp4' })
+    const objects = '<1/0>, <3/0>, <6/0>'
+    const deviceName = 'device_name'
+    const lifetime = '3600'
+    const lwm2mV = "1.1"
+    const biding = "U"
+    const port = "5683"
+    const host = "localhost"
 
     beforeEach(function (done) {
-        port = 5683
         server = createServer()
-        server.listen(port, done)
+        server.listen(5683, done)
     })
 
 
@@ -46,14 +53,23 @@ describe("handshake", () => {
     })
 
     it("should send handshake request to server with agent", async () => {
-        const agent = new coap.Agent({ type: 'udp4' })
-        const objects = '<1/0>, <3/0>, <6/0>'
-        const deviceName = 'device_name'
-        const lifetime = '3600'
-        const lwm2mV = "1.1"
-        const biding = "U"
-        const port = "5683"
-        const host = "localhost"
+        handShake(agent, objects, deviceName, lifetime, lwm2mV, biding, port, host) 
+
+        const serverReceive: Promise<{url: string; payload:string}> = new Promise((resolve, reject) => {
+            server.on('request', (req, res) => {
+                //console.log('Hanshake request sent. This is received by the server here')
+                res.end()
+                resolve({url:req.url, payload:req.payload.toString()})
+            })
+        })
+
+        const result = await serverReceive
+        expect(result.url).toBe('/rd?ep=device_name&lt=3600&lwm2m=1.1&b=U')
+        expect(result.payload).toBe(`</>;ct=11543;hb,${objects}`)
+    })
+
+    it("should receive answer from server after send the handshake request", async () => {
+        
         handShake(agent, objects, deviceName, lifetime, lwm2mV, biding, port, host) 
 
         const serverReceive: Promise<{url: string; payload:string}> = new Promise((resolve, reject) => {
