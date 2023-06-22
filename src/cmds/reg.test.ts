85b27e67ceb8d178549e79df967134901675ac8e
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-06-22 14:52:49 +0200

    refactor: registerDeviceObjects test

--- src/cmds/reg.test.ts
+++ src/cmds/reg.test.ts
@@ -53,4 +53,51 @@ describe('registerDeviceObjects', () => {
 		},
 		12 * 1000,
 	)
+
+	it.only('should register device objects', async () => {
+		const requestResourceValue = (socketPort: number, resource: string) => {
+			const params = {
+				host: 'localhost',
+				port: socketPort,
+				pathname: resource,
+				method: 'GET' as CoapMethod,
+				options: {
+					'Content-Format': 'application/link-format',
+				},
+			}
+			return request(params).end()
+		}
+
+		const serverExecutionResult = new Promise<{ socketPort: number }>(
+			(resolve) => {
+				// receive client handshake
+				server.on('request', (req, res) => {
+					res.end()
+					resolve({ socketPort: req.rsinfo.port })
+				})
+			},
+		)
+			.then(
+				async ({ socketPort }) =>
+					// Request resource value to the client
+					new Promise<OutgoingMessage>((resolve) =>
+						resolve(requestResourceValue(socketPort, '/3/0/0')),
+					),
+			)
+			.then(
+				async (client) =>
+					// wait for client's response
+					new Promise<void>((resolve) => {
+						client.on('response', (req) => {
+							resolve(req.payload.toString())
+						})
+					}),
+			)
+
+		await registerDeviceObjects(assetTrackerFirmwareV2)
+
+		expect(await serverExecutionResult).toBe(
+			`{"bn":"/3/0/0","e":[{"sv":"Nordic"}]}`,
+		)
+	})
 })
