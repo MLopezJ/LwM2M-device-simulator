ccb3d58cb59cf2fa26764bbdbd91961bda8c6050
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-06-22 16:13:44 +0200

    refactor: check instance tests

--- src/utils/checkInstance.test.ts
+++ src/utils/checkInstance.test.ts
@@ -6,27 +6,33 @@ import { assetTrackerFirmwareV2 } from '../assetTrackerV2.js'
 import { checkInstance } from './checkInstance.js'
 
 describe('Check Instance', () => {
-	it('Should check that instance exist in object (single instance object)', () => {
-		const list = assetTrackerFirmwareV2
-		const result = checkInstance(list[Device_3_urn], 0)
-		expect(result).toMatchObject(list[Device_3_urn] ?? '')
+	describe('single instance', () => {
+		it('Should return instance if it exists', () => {
+			const list = assetTrackerFirmwareV2
+			const result = checkInstance(list[Device_3_urn], 0)
+			const instance = list[Device_3_urn] ?? ''
+			expect(result).toMatchObject(instance)
+		})
+
+		it('Should return undefined when instance does not exist in object', () => {
+			const list = assetTrackerFirmwareV2
+			const result = checkInstance(list[Device_3_urn], 30)
+			expect(result).toBe(undefined)
+		})
 	})
 
-	it('Should check that instance exist in object (multiple instance object)', () => {
-		const list = assetTrackerFirmwareV2
-		const result = checkInstance(list[Temperature_3303_urn], 0)
-		expect(result).toMatchObject(list[Temperature_3303_urn]?.[0] ?? '')
-	})
+	describe('multiple instance', () => {
+		it('Should return instance if it exists', () => {
+			const list = assetTrackerFirmwareV2
+			const result = checkInstance(list[Temperature_3303_urn], 0)
+			const instance = list[Temperature_3303_urn]?.[0] ?? ''
+			expect(result).toMatchObject(instance)
+		})
 
-	it('Should inform that instance do not exist in object (single instance object)', () => {
-		const list = assetTrackerFirmwareV2
-		const result = checkInstance(list[Device_3_urn], 30)
-		expect(result).toBe(undefined)
-	})
-
-	it('Should inform that instance do not exist in object (multiple instance object)', () => {
-		const list = assetTrackerFirmwareV2
-		const result = checkInstance(list[Temperature_3303_urn], 30)
-		expect(result).toBe(undefined)
+		it('Should return undefined when instance does not exist in object', () => {
+			const list = assetTrackerFirmwareV2
+			const result = checkInstance(list[Temperature_3303_urn], 30)
+			expect(result).toBe(undefined)
+		})
 	})
 })
