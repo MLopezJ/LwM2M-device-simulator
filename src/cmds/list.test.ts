7a96d0f26c97dfbf11d5ff5b95d16cdfe7abf9a7
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-06-22 15:55:14 +0200

    refactor: list test

--- src/cmds/list.test.ts
+++ src/cmds/list.test.ts
@@ -6,59 +6,70 @@ import { assetTrackerFirmwareV2 } from '../assetTrackerV2.js'
 import { list } from '../cmds/list.js'
 
 describe('List', () => {
+	let objectList = assetTrackerFirmwareV2
+	beforeEach(() => {
+		objectList = assetTrackerFirmwareV2
+	})
+
 	it('Should list all the object list when no URL is provided', async () => {
 		const param = undefined
-		expect(await list(param, assetTrackerFirmwareV2)).toMatchObject(
-			assetTrackerFirmwareV2,
-		)
-	})
-
-	it('Should list object (single instance)', async () => {
-		const param = '/3'
-		expect(await list(param, assetTrackerFirmwareV2)).toMatchObject(
-			assetTrackerFirmwareV2[Device_3_urn] ?? '',
-		)
-	})
-
-	it('Should return undefined when request for a no existing instance of a single instance object', async () => {
-		const param = '/3/1'
-		expect(await list(param, assetTrackerFirmwareV2)).toBe(undefined)
-	})
-
-	it('Should list object (multiple instance)', async () => {
-		const param = '/3303'
-		expect(await list(param, assetTrackerFirmwareV2)).toMatchObject(
-			assetTrackerFirmwareV2[Temperature_3303_urn] ?? '',
-		)
-	})
-
-	it('Should return undefined when request for a no existing instance of a multiple instance object', async () => {
-		const param = '/3303/10'
-		expect(await list(param, assetTrackerFirmwareV2)).toBe(undefined)
-	})
-
-	it('Should list resource (single instance object)', async () => {
-		const param = '/3/0/0'
-		expect(await list(param, assetTrackerFirmwareV2)).toBe('Nordic')
-	})
-
-	it('Should return undefined when request for a no existing resource of a single instance object', async () => {
-		const param = '/3/0/1010101010'
-		expect(await list(param, assetTrackerFirmwareV2)).toBe(undefined)
-	})
-
-	it('Should list resource (multiple instance)', async () => {
-		const param = '/3303/0/5700'
-		expect(await list(param, assetTrackerFirmwareV2)).toBe(24.57)
-	})
-
-	it('Should return undefined when request for a no existing resource of a multiple instance object', async () => {
-		const param = '/3303/0/1010101010'
-		expect(await list(param, assetTrackerFirmwareV2)).toBe(undefined)
+		expect(await list(param, objectList)).toMatchObject(objectList)
 	})
 
 	it('Should return undefined when input do not follow LwM2M object syntax', async () => {
 		const param = 'test'
-		expect(await list(param, assetTrackerFirmwareV2)).toBe(undefined)
+		const expectedResult = undefined
+		expect(await list(param, objectList)).toBe(expectedResult)
+	})
+
+	describe('single instance', () => {
+		it('Should list object', async () => {
+			const param = '/3'
+			const expectedResult = objectList[Device_3_urn] ?? ''
+			expect(await list(param, objectList)).toMatchObject(expectedResult)
+		})
+
+		it('Should list resource', async () => {
+			const param = '/3/0/0'
+			const expectedResult = 'Nordic'
+			expect(await list(param, objectList)).toBe(expectedResult)
+		})
+
+		it('Should return undefined when request for a no existing instance', async () => {
+			const param = '/3/1'
+			expect(await list(param, assetTrackerFirmwareV2)).toBe(undefined)
+		})
+
+		it('Should return undefined when request for a no existing resource', async () => {
+			const param = '/3/0/1010101010'
+			const expectedResult = undefined
+			expect(await list(param, objectList)).toBe(expectedResult)
+		})
+	})
+
+	describe('multiple instance', () => {
+		it('Should list object', async () => {
+			const param = '/3303'
+			const expectedResult = objectList[Temperature_3303_urn] ?? ''
+			expect(await list(param, objectList)).toMatchObject(expectedResult)
+		})
+
+		it('Should list resource', async () => {
+			const param = '/3303/0/5700'
+			const expectedResult = 24.57
+			expect(await list(param, objectList)).toBe(expectedResult)
+		})
+
+		it('Should return undefined when request for a no existing instance', async () => {
+			const param = '/3303/10'
+			const expectedResult = undefined
+			expect(await list(param, objectList)).toBe(expectedResult)
+		})
+
+		it('Should return undefined when request for a no existing resource', async () => {
+			const param = '/3303/0/1010101010'
+			const expectedResult = undefined
+			expect(await list(param, objectList)).toBe(expectedResult)
+		})
 	})
 })
