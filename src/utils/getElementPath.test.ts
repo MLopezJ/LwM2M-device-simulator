f69d79ea1f8268f288cc78d87856300031c0a462
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-06-23 09:46:02 +0200

    test: add more examples

--- src/utils/getElementPath.test.ts
+++ src/utils/getElementPath.test.ts
@@ -3,6 +3,8 @@ import { getElementPath } from './getElementPath.js'
 describe('getElementPath', () => {
 	it.each([
 		['/4', { objectId: 4, instanceId: undefined, resourceId: undefined }],
+		['/6', { objectId: 6, instanceId: undefined, resourceId: undefined }],
+		['/3304/0', { objectId: 3304, instanceId: 0, resourceId: undefined }],
 		['/3303/2', { objectId: 3303, instanceId: 2, resourceId: undefined }],
 		['/3/0/1', { objectId: 3, instanceId: 0, resourceId: 1 }],
 		['/3303/10/5700', { objectId: 3303, instanceId: 10, resourceId: 5700 }],
